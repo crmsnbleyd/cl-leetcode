a4ff57de2d306a5dcf32e94fdb5d43b9dca77308
Andrew <arnav.jose@gmail.com>
2022-09-20 22:28:31 +0530

    Added solution for leetcode question 3

--- solutions/two-sum.lisp
+++ solutions/two-sum.lisp
@@ -1,8 +1,9 @@
 (defun two-sum (nums target)
-  (let ((hash (make-hash-table)))
-     (loop for x in nums and idx from 0
-	   do (setf (gethash x hash) (cons idx (gethash x hash))))
-     (loop for k being each hash-key of hash
-	   do (when (gethash (- target k) hash)
-		(unless (and (= target (* k 2)) (not (cdr (gethash k hash))))
-		(return-from two-sum (car (gethash (- target k) hash))))))))
+  (let ((hash (make-hash-table))
+	(iter-hash-item nil))
+    (loop for x in nums and idx from 0
+	  do (setf (gethash x hash) (cons idx (gethash x hash))))
+    (loop for k being each hash-key of hash
+	  do (when (setf iter-hash-item (gethash (- target k) hash))
+	       (unless (and (= target (* k 2)) (not (cdr iter-hash-item)))
+		 (return-from two-sum (car iter-hash-item)))))))
