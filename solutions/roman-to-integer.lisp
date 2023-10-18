051182065e2d39fa6ab07481e7c511dbef31312d
Andrew Jose <arnav.jose@gmail.com>
2023-10-18 16:53:21 +0530

    bonus solution: roman to integer

---
+++ solutions/roman-to-integer.lisp
@@ -0,0 +1,34 @@
+(defun string-prefix? (prefix str)
+  (if (< (length str) (length prefix))
+      nil
+      (loop :for pchar :across prefix
+	    :for schar :across str
+	    :when (char/= pchar schar)
+	      :return nil
+	    :finally (return t))))
+
+(defun integer-to-roman (roman-string)
+  (loop :with sum = 0
+	:with str = roman-string
+	:do
+	   (loop :for (prefix . value)
+		   :in
+		 '(("IV" . 4)
+		   ("IX" . 9)
+		   ("XL" . 40)
+		   ("XC" . 90)
+		   ("CD" . 400)
+		   ("CM" . 900)
+		   ("I"  . 1)
+		   ("V"  . 5)
+		   ("X"  . 10)
+		   ("L"  . 50)
+		   ("C"  . 100)
+		   ("D"  . 500)
+		   ("M"  . 1000))
+		 :when (string-prefix? prefix str)
+		   :do (setf sum (+ sum value))
+		       (setf str (subseq str (length prefix)))
+		       (return))
+	:when (string= str "")
+	  :return sum))
